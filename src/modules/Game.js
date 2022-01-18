e18f5d336d1244cf9dd5e48c911f1b3e4017ba64
Imal16 <malek.ihsaan@gmail.com>
2022-01-17 22:32:33 -0500

    Started working

---
+++ src/modules/Game.js
@@ -0,0 +1,21 @@
+import TileMap from "./TileMap.js";
+
+
+const tileSize = 32;                                    //Each square in the map is 32 pixles
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext('2d');
+const gameMap = new TileMap(tileSize);
+
+
+
+// Redraw the screen certain amount of times every 1 second
+function gameLoop(){
+    //TileMap.draw();
+
+
+
+
+}
+
+//Call the function every x period of time, call function 75 times every 1000ms or 1 second
+setInterval(gameLoop, 1000 / 75);
